1d1468ffac8337d8fac7e078bd3107b9cd7934e7
Lautaro Iezzi <iezzilautaro@gmail.com>
2026-01-09 11:58:54 -0300

    fix: ssl robusto

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,23 +3,22 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// Configuración del Pool de conexiones
+// Detectamos producción si el Host de la DB NO es localhost
+const isProduction = process.env.DB_HOST && process.env.DB_HOST !== 'localhost';
+
 const pool = new pg.Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-    // 👇 ESTO ES LO QUE FALTA:
-    // Si existe la variable RAILWAY_ENVIRONMENT, activamos SSL. Si no, lo dejamos apagado (local).
-    ssl: process.env.RAILWAY_ENVIRONMENT ? { rejectUnauthorized: false } : false
+    // 👇 ESTA ES LA LÓGICA INFALIBLE:
+    ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
-// Listener para errores de conexión
 pool.on('error', (err) => {
     console.error('❌ Error inesperado en PostgreSQL', err);
     process.exit(-1);
 });
 
-// Función query exportada
 export const query = (text, params) => pool.query(text, params);
