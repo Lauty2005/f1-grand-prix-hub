a9c09b27f058fe60426fa977b4b4ae1aad6015db
Lautaro Iezzi <iezzilautaro@gmail.com>
2026-01-09 11:45:42 -0300

    fix: ssl database y package type

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,12 +3,17 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// Detectamos si hay un puerto definido (típico de la nube) para activar SSL
+const isProduction = !!process.env.RAILWAY_ENVIRONMENT || process.env.NODE_ENV === 'production';
+
 const pool = new pg.Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    // 👇 ESTA ES LA CLAVE: SSL activado para Railway, desactivado para Localhost
+    ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 // Listener para errores inesperados en la conexión
